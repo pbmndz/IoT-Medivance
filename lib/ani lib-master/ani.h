9c746c015b0f455dd668104f12c8e6c2bf3b0b75
pbmndz <pbmendoza0916@gmail.com>
2024-06-18 01:13:28 +0800

    Merge remote changes

---
+++ lib/ani lib-master/ani.h
@@ -0,0 +1,55 @@
+#ifndef ani_h
+#define ani_h
+
+#include "Arduino.h"
+
+class ani {
+  public:
+    static const unsigned char epd_bitmap__a_frm0_30[];
+    static const unsigned char epd_bitmap__a_frm1_30[];
+    static const unsigned char epd_bitmap__a_frm2_30[];
+    static const unsigned char epd_bitmap__a_frm3_30[];
+    static const unsigned char epd_bitmap__a_frm4_30[];
+    static const unsigned char epd_bitmap__a_frm5_30[];
+    static const unsigned char epd_bitmap__a_frm6_30[];
+    static const unsigned char epd_bitmap__a_frm7_30[];
+    static const unsigned char epd_bitmap__a_frm8_30[];
+    static const unsigned char epd_bitmap__a_frm9_30[];
+    static const unsigned char epd_bitmap__a_frm10_30[];
+    static const unsigned char epd_bitmap__a_frm11_30[];
+    static const unsigned char epd_bitmap__a_frm12_30[];
+    static const unsigned char epd_bitmap__a_frm13_30[];
+    static const unsigned char epd_bitmap__a_frm14_30[];
+    static const unsigned char epd_bitmap__a_frm15_30[];
+    static const unsigned char epd_bitmap__a_frm16_30[];
+    static const unsigned char epd_bitmap__a_frm17_30[];
+    static const unsigned char epd_bitmap__a_frm18_30[];
+    static const unsigned char epd_bitmap__a_frm19_30[];
+    static const unsigned char epd_bitmap__a_frm20_30[];
+    static const unsigned char epd_bitmap__a_frm21_30[];
+    static const unsigned char epd_bitmap__a_frm22_30[];
+    static const unsigned char epd_bitmap__a_frm23_30[];
+    static const unsigned char epd_bitmap__a_frm24_30[];
+    static const unsigned char epd_bitmap__a_frm25_30[];
+    static const unsigned char epd_bitmap__a_frm26_30[];
+    static const unsigned char epd_bitmap__a_frm27_30[];
+    static const unsigned char epd_bitmap__a_frm28_30[];
+    static const unsigned char epd_bitmap__a_frm29_30[];
+    static const unsigned char epd_bitmap__a_frm30_30[];
+    static const unsigned char epd_bitmap__a_frm31_30[];
+    static const unsigned char epd_bitmap__a_frm32_30[];
+    static const unsigned char epd_bitmap__a_frm33_30[];
+    static const unsigned char epd_bitmap__a_frm34_30[];
+    static const unsigned char epd_bitmap__a_frm35_30[];
+    static const unsigned char epd_bitmap__a_frm36_30[];
+    static const unsigned char epd_bitmap__a_frm37_30[];
+    static const unsigned char epd_bitmap__a_frm38_30[];
+    static const unsigned char epd_bitmap__a_frm39_30[];
+    static const unsigned char epd_bitmap__a_frm40_30[];
+    static const unsigned char epd_bitmap__a_frm41_30[];
+    static const unsigned char* epd_bitmap_allArray[42];
+    static const unsigned char* getBitmap(int index);
+    static const int getBitmapArrayLength();
+};
+
+#endif
